25c70916def905484f5792629eb0df5dbb1b2aac
Michael Marquez <mmarquez@circleci.com>
2019-12-14 19:27:29 -0500

    Change name to calculation

--- lib/fuel.py
+++ lib/fuel.py
@@ -1,5 +1,5 @@
 import math
 
-def calculator(mass):
-    """Calculates the amount of fuel required to launch a module"""
+def _calculation(mass):
+    """Calculates the amount of fuel required to launch"""
     return math.floor(mass / 3) - 2
