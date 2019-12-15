e528b370220e0b4b71d4ac3039e09b6e6ff2e9c9
Michael Marquez <mmarquez@circleci.com>
2019-12-14 19:31:14 -0500

    Add function and tests to calculate the total fuel required
    Apparently, we forgot to include additional fuel for the fuel you just added.

--- lib/fuel.py
+++ lib/fuel.py
@@ -3,3 +3,12 @@ import math
 def _calculation(mass):
     """Calculates the amount of fuel required to launch"""
     return math.floor(mass / 3) - 2
+
+def total(mass):
+    """Returns the total amount fuel required"""
+    fuel_weights = []
+    while mass >= 0:
+        mass = _calculation(mass)
+        if mass > 1:
+          fuel_weights.append(mass)
+    return sum(fuel_weights)
