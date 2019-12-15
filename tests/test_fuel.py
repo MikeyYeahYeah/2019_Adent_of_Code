20e5165af91119efa9bf240e5402fc2ce77a3892
Michael Marquez <mmarquez@circleci.com>
2019-12-14 20:49:35 -0500

    Change test function name to match actual function name

--- tests/test_fuel.py
+++ tests/test_fuel.py
@@ -8,7 +8,7 @@ def test__calculation():
     assert fuel._calculation(1969) == 654
     assert fuel._calculation(100756) == 33583
 
-def test_total_fuel():
+def test_total():
     assert fuel.total(14) == 2
     assert fuel.total(1969) == 966
     assert fuel.total(100756) == 50346
